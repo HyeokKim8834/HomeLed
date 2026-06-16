42c94a6df4217142b06f4392644f95bf9714f866
Hyeok <kimhyuk813@gmail.com>
2026-06-15 20:12:54 -0400

    Revised web server

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <SPIFFS.h> 
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
@@ -12,24 +13,31 @@ void setup() {
   Serial.begin(115200);
   WiFi.begin(ssid, password);
   
-  Serial.print("WiFi 연결 중");
+  Serial.print("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   
-  Serial.println("\n연결 완료!");
-  Serial.print("IP 주소: ");
+  Serial.println("\nConnection complete!");
+  Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
- // "text/plain; charset=utf-8" 로 인코딩 명시해줘야 한글이 깨지지 않음
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(200, "text/plain; charset=utf-8", "ESP32 LED 서버 작동 중!");
-    });
+  // SPIFFS 초기화 - 실패하면 시리얼 모니터에 에러 출력
+  if (!SPIFFS.begin(true)) {
+    Serial.println("SPIFFS Initialization Failed!");
+    return;
+  }
+  Serial.println("SPIFFS Initialization Successful!");
+
+    // 텍스트 대신 data/index.html 파일을 브라우저에 보내줌
+      server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+      request->send(SPIFFS, "/index.html", "text/html");
+      });
 
     // 서버 시작 - 이 줄 이후부터 브라우저 접속 가능
     server.begin();
-    Serial.println("웹 서버 시작됨");
+    Serial.println("web server started");
 }
 
 void loop() {
